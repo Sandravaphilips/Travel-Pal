98cc7e7fc948dd239037b9dffd33f7799e97ba2f
Sandravaphilips <lacasandra1@gmail.com>
2020-11-20 07:50:46 -0800

    added onClick handler

--- client/src/components/SearchTab.js
+++ client/src/components/SearchTab.js
@@ -1,11 +1,11 @@
 import { Button, FormControl, Input, InputAdornment } from '@material-ui/core';
 import { SearchOutlined } from '@material-ui/icons';
 
-const SearchTab =  ({searchTerm, onChange}) => {
+const SearchTab =  ({searchTerm, onChange, onClick}) => {
   return(
     <FormControl variant='outlined' className='search'>
       <Input startAdornment={<InputAdornment position='start'><SearchOutlined /></InputAdornment>} placeholder='Search for your next country of travel...' value={searchTerm} onChange={onChange} />
-      <Button size='small' variant="contained" color="primary">Search</Button>
+      <Button size='small' variant="contained" color="primary" onClick={onClick}>Search</Button>
     </FormControl>
   )
 };
