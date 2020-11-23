6fc9335e445dce50c28fe70e3adbe767573bf952
Sandravaphilips <lacasandra1@gmail.com>
2020-11-23 08:39:58 -0800

    moved the Home component to App and deleted Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,41 @@
-import { Route } from "react-router-dom";
+import { useState } from "react";
+import Axios from 'axios';
+
+import SearchTab from "./components/SearchTab";
+import ResultsTab from "./components/ResultsTab";
+import Navigation from "./components/Navigation";
+import useLocalStorage from "./hooks/useLocalStorage";
 import './App.css';
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import Home from "./components/Home";
 
-function App() {
+const App = () => {
+  const [ searchTerm, setSearchTerm ] = useState('');
+  const [ searchResults, setSearchResults ] = useState([]);
+  const [ history, setHistory ] = useLocalStorage('history', null);
 
+  const onChange = e => {
+    setSearchTerm(e.target.value)
+  };
+
+  const handleClick = e => {
+    e.preventDefault()
+    setHistory([])
+    if(searchTerm) {
+      Axios.get(`http://localhost:5000/${searchTerm}`)
+      .then(({ data }) => {
+        setSearchResults(data)
+        setHistory(history.push(searchTerm))
+      })
+      .catch(err => console.log(err))
+    } 
+  }
+  
   return (
     <div className="App">
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/signup' component={SignUp} />
+      <Navigation />
+      <main>
+        <SearchTab searchTerm={searchTerm} onChange={onChange} onClick={handleClick}/>
+        <ResultsTab history={history} searchResults={searchResults} setSearchResults={setSearchResults} searchTerm={searchTerm}/>
+      </main>
     </div>
   );
 }
