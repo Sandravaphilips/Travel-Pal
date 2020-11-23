99c592419232db244810072969bf4bf718e598c3
Sandravaphilips <lacasandra1@gmail.com>
2020-11-23 08:38:32 -0800

    cleared out the login button

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,11 +1,10 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import { Link } from "react-router-dom";
+
 const Navigation = () => {
     return(
         <AppBar position="static">
             <Toolbar>
                 <Typography variant='h6' style={{flexGrow: 1}}>Travel Pal</Typography>
-                <Link to='/login' className='logout'>Login</Link>
             </Toolbar>
         </AppBar>
     )
