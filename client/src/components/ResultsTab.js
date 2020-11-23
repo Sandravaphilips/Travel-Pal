2d74f8635cf24ef383123649c0d0e10192d37c03
Sandravaphilips <lacasandra1@gmail.com>
2020-11-23 08:40:16 -0800

    cleaned up code

--- client/src/components/ResultsTab.js
+++ client/src/components/ResultsTab.js
@@ -2,28 +2,40 @@
 //import Axios from "axios";
 import ResultTab from "./ResultTab";
 
-const ResultsTab = ({ searchTerm, setSearchResults, searchResults }) => {
-    // useEffect(() => {
-    //     if(searchTerm) {
-    //         Axios.get(`http://localhost:5000/${searchTerm}`)
-    //         .then(({ data }) => {
-    //           setSearchResults(data)
-    //         })
-    //         .catch(err => console.log(err))
-    //       }      
-    // }, [searchTerm, setSearchResults])
+const ResultsTab = ({ searchTerm, searchResults, history }) => {
+
+    const renderedComponent = () => {
+        if(searchResults.length === 0 && history.length > 0) {
+            return(
+                <div>
+                    <h2>
+                        Countries You've Previously Searched:
+                    </h2>
+                    {
+                        history.map((country, indx) => 
+                            <ResultTab {...country} key={indx} />
+                        )
+                    }
+                </div>
+            )
+        } else if(searchResults.length > 0) {
+            return(
+                <div>
+                    <h2>Results for "{searchTerm}"</h2>
+                    {
+                        searchResults.map((result, indx) => 
+                            <ResultTab {...result} key={indx} />
+                        )
+                    }
+                </div>
+            )
+        } else return(
+            <h3>Type in your next country of travel! </h3>
+        )
+    }
 
     return(
-        <div>
-            <h2>Results for "{searchTerm}"</h2>
-            {
-                searchResults ?
-                searchResults.map((result, indx) => 
-                    <ResultTab {...result} key={indx} />
-                ) :
-                'Start searching'
-            }
-        </div>
+        renderedComponent()
     )
 };
 
