b6d3c00d9521fdc09e497631cbe6a467091bc6e7
Sandravaphilips <lacasandra1@gmail.com>
2020-11-26 05:31:22 -0800

    added light styling to the FormControl

--- client/src/components/SearchTab.js
+++ client/src/components/SearchTab.js
@@ -1,11 +1,11 @@
-import { Button, FormControl, Input, InputAdornment } from '@material-ui/core';
+import { Button, FormControl, InputAdornment, OutlinedInput } from '@material-ui/core';
 import { SearchOutlined } from '@material-ui/icons';
 
 const SearchTab =  ({searchTerm, onChange, onClick}) => {
   return(
-    <FormControl variant='outlined' className='search'>
-      <Input startAdornment={<InputAdornment position='start'><SearchOutlined /></InputAdornment>} placeholder='Search for your next country of travel...' value={searchTerm} onChange={onChange} />
-      <Button size='small' variant="contained" color="primary" onClick={onClick}>Search</Button>
+    <FormControl fullWidth variant='outlined' className='search'>
+      <OutlinedInput startAdornment={<InputAdornment position='start'><SearchOutlined /></InputAdornment>} placeholder='Search for your next country of travel...' value={searchTerm} onChange={onChange} />
+      <Button size='large' className='btn' variant="contained" color="primary" onClick={onClick}>Search</Button>
     </FormControl>
   )
 };
