01ba7d6a6c0fc1931a62336c6102c45d468bad6a
Sandravaphilips <lacasandra1@gmail.com>
2020-11-27 08:10:09 -0800

    added a KeyPress function for Enter

--- client/src/components/SearchTab.js
+++ client/src/components/SearchTab.js
@@ -2,9 +2,15 @@ import { Button, FormControl, InputAdornment, OutlinedInput } from '@material-ui
 import { SearchOutlined } from '@material-ui/icons';
 
 const SearchTab =  ({searchTerm, onChange, onClick}) => {
+  const handleKeypress = e => {
+    if (e.nativeEvent.keyCode === 13) {
+      onClick(e);
+    }
+  };
+
   return(
     <FormControl fullWidth variant='outlined' className='search'>
-      <OutlinedInput startAdornment={<InputAdornment position='start'><SearchOutlined /></InputAdornment>} placeholder='Search for your next country of travel...' value={searchTerm} onChange={onChange} />
+      <OutlinedInput onKeyPress={handleKeypress} startAdornment={<InputAdornment position='start'><SearchOutlined /></InputAdornment>} placeholder='Search for your next country of travel...' value={searchTerm} onChange={onChange} />
       <Button size='large' className='btn' variant="contained" color="primary" onClick={onClick}>Search</Button>
     </FormControl>
   )
